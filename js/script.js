6982cfb6f29e987aa817c139fd0d1df26542cf0a
Srinivas <125016918+NelloreSrinivaskumar@users.noreply.github.com>
2023-10-12 22:50:20 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+burger = document.querySelector('.burger')
+navBar = document.querySelector('.navbar')
+navList = document.querySelector('navlist')
+rightNav = document.querySelector('.rightnav')
+
+
+
+
+burger.addEventListener('click', ()=>{
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navBar.classList.toggle('h-nav-resp');
+})
